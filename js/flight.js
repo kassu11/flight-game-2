549e0d1224e1a6af4e3ae0adc4d8192a5af00a38
Aihki <laine.nestori.julius@gmail.com>
2022-11-30 10:53:11 +0200

    Toimiva kartta [#2]
    Tehtii kartta joka näyttää mihin lentokentälle päädyit

--- js/flight.js
+++ js/flight.js
@@ -1,9 +1,30 @@
 let airportCountry = prompt('Enter the starting country code:');
+const map = L.map('map')
 
 async function kutsu(airportCountry) {
-  const answer = await fetch(`http://127.0.0.1:3000/code/${airportCountry}`);
-  const jsonAnswer = await answer.json();
-  console.log(jsonAnswer);
+    const answer = await fetch(`http://127.0.0.1:3000/code/${airportCountry}`);
+    const jsonAnswer = await answer.json();
+    console.log(jsonAnswer);
+    updateMap(jsonAnswer)
+
+}
+
+function updateMap(jsonAnswer) {
+    const longitude_deg = jsonAnswer.longitude_deg
+    const latitude_deg = jsonAnswer.latitude_deg
+    map.setView([latitude_deg, longitude_deg], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude_deg, longitude_deg]).addTo(map)
+        .bindPopup(jsonAnswer.name)
+        .openPopup();
 }
 
 kutsu(airportCountry);
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import mysql.connector
 connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    database='flight_game1',
+    database='flight_game',
     user='root',
-    password='Rommikola77',
+    password='T4!d4R!mz',
     autocommit=True
 )
 
@@ -19,11 +19,11 @@ app.config["cors_headers"] = "content-type"
 
 @app.route("/code/<maakoodi>")
 def get_country(maakoodi):
-    sql = f"SELECT name, iso_country, municipality FROM airport WHERE iso_country='{maakoodi}';"
+    sql = f"SELECT name, iso_country, municipality, latitude_deg, longitude_deg FROM airport WHERE iso_country='{maakoodi}';"
     cursor = connection.cursor()
     cursor.execute(sql)
     result = random.choice(cursor.fetchall())
-    return {"icao": result[1], "name": result[0], "municipality": result[2]}
+    return {"icao": result[1], "name": result[0], "municipality": result[2], "latitude_deg": result[3], "longitude_deg": result[4] }
 
 
 if __name__ == "__main__":
