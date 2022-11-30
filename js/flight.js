a044ece79688856d0800e02c991e9db100bffd2f
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-11-30 13:58:31 +0200

    Pelaajan teko päätepolku [#3]
    - Tein function pohjan, josta on hyvä jatkaa huomenna ;D

--- js/flight.js
+++ js/flight.js
@@ -1,30 +1,39 @@
-let airportCountry = prompt('Enter the starting country code:');
-const playerName = prompt('Enter your name :');
+
+
 const map = L.map('map')
 
-async function kutsu(airportCountry) {
+async function airportByIsocode(airportCountry) {
     const answer = await fetch(`http://127.0.0.1:3000/code/${airportCountry}`);
     const jsonAnswer = await answer.json();
     console.log(jsonAnswer);
     updateMap(jsonAnswer)
-
+    return jsonAnswer;
 }
 
-async function get_Name(playerName) {
-    const nameAnswer = await fetch(`http://127.0.0.1:3000/newplayer/${playerName}`)
-    const data = await nameAnswer.text()
-    console.log(data);
+// async function get_Name(playerName) {
+//     const nameAnswer = await fetch(`http://127.0.0.1:3000/newplayer/${playerName}`)
+//     const data = await nameAnswer.text()
+//     console.log(data);
+// }
+
+async function addPlayer() {
+    const playerName = prompt("Enter your name :");
+    const startIsoCode = prompt('Enter the starting country code:');
+
+    const airportData = await airportByIsocode(startIsoCode);
+    fetch("http://127.0.0.1:3000/newplayer", {
+        method: "POST",
+        body: JSON.stringify({airport: airportData, playerName: playerName})
+    });
 }
- get_Name(playerName)
+// get_Name(playerName)
 
 
 function updateMap(jsonAnswer) {
     const longitude_deg = jsonAnswer.longitude_deg
     const latitude_deg = jsonAnswer.latitude_deg
     map.setView([latitude_deg, longitude_deg], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {attribution: ""}).addTo(map);
 
     L.marker([latitude_deg, longitude_deg]).addTo(map)
         .bindPopup(jsonAnswer.name)
@@ -32,10 +41,10 @@ function updateMap(jsonAnswer) {
 }
 
 
+addPlayer()
 
 
-
-kutsu(airportCountry);
+// airportByIsocode(airportCountry);
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import random
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, request
 import mysql.connector
 
 
 class Player:
-
     def __init__(self, id, nimi, airport, co2, score):
         self.id = id
         self.nimi = nimi
@@ -18,22 +17,15 @@ class Player:
 connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    database='flight_game',
+    database='flight_game1',
     user='root',
-    password='portland',
+    password='Salasana1',
     autocommit=True
 )
 
 app = Flask(__name__)
 CORS(app)
 app.config["cors_headers"] = "content-type"
-
-
-@app.route("/newplayer/<name>")
-def give_name(name):
-    newplayer = Player(1, name, "", 0, 0)
-    print(newplayer.nimi)
-    return "ok"
 
 
 @app.route("/code/<maakoodi>")
@@ -43,6 +35,15 @@ def get_country(maakoodi):
     cursor.execute(sql)
     result = random.choice(cursor.fetchall())
     return {"icao": result[1], "name": result[0], "municipality": result[2], "latitude_deg": result[3], "longitude_deg": result[4]}
+
+
+@app.route("/newplayer", methods=["POST"])
+def testi():
+    if request.method == "POST":
+        player_data = request.get_json(force=True)
+        print(player_data["playerName"])
+        print(player_data)
+        return {"status": "ok"}
 
 
 if __name__ == "__main__":
