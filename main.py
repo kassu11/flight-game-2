e8bba6a8f2403c72877afb5a53f22d4adf2d9624
Aihki <laine.nestori.julius@gmail.com>
2022-12-01 12:27:08 +0200

    pelaajan claasin viimeistely [#3]
    tein loppuun uuden pelaajan luonnin.

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ from flask import Flask, request
 import mysql.connector
 
 
-class Player:
-    def __init__(self, id, nimi, airport, co2, score):
-        self.id = id
-        self.nimi = nimi
-        self.airport = airport
-        self.co2 = co2
-        self.score = score
-        self.co2max = 100000
-
-
 connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    database='flight_game1',
+    database='flight_game',
     user='root',
-    password='Salasana1',
+    password='T4!d4R!mz',
     autocommit=True
 )
+cursor = connection.cursor(buffered=True)
+
+class Player:
+
+    def __init__(self, nimi, airport):
+        cursor.execute("SELECT max(CAST(id AS INT)) FROM game")
+        id_result = cursor.fetchone()
+        if id_result == None:
+            self.id = 1
+        else:
+            self.id = id_result + 1
+        self.nimi = nimi
+        self.airport = airport
+        self.co2 = 0
+        self.score = 0
+        self.co2max = 100000
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -31,18 +39,18 @@ app.config["cors_headers"] = "content-type"
 @app.route("/code/<maakoodi>")
 def get_country(maakoodi):
     sql = f"SELECT name, iso_country, municipality, latitude_deg, longitude_deg FROM airport WHERE iso_country='{maakoodi}';"
-    cursor = connection.cursor()
     cursor.execute(sql)
     result = random.choice(cursor.fetchall())
     return {"icao": result[1], "name": result[0], "municipality": result[2], "latitude_deg": result[3], "longitude_deg": result[4]}
 
 
 @app.route("/newplayer", methods=["POST"])
-def testi():
+def new_player():
     if request.method == "POST":
         player_data = request.get_json(force=True)
         print(player_data["playerName"])
         print(player_data)
+        Player(player_data["playerName"], player_data["airportData"])
         return {"status": "ok"}
 
 
