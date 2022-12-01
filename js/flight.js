b7a4cb093e431467a8f1175c9f036f5fb4324dcb
Jambolul <jami.naatanen@gmail.com>
2022-12-01 13:37:39 +0200

    Edistetty käyttöliittymää [#4]
    Player classia korjattu. Luotu napit, airport endpoint. Nappeihin sijoitettu lentokentät, asettelua sivulle.

--- js/flight.js
+++ js/flight.js
@@ -1,43 +1,46 @@
-let airportCountry = prompt('Enter the starting country code:');
-const playerName = prompt('Enter your name :');
+
+
 const map = L.map('map')
 
-async function kutsu(airportCountry) {
+async function airportByIsocode(airportCountry) {
     const answer = await fetch(`http://127.0.0.1:3000/code/${airportCountry}`);
     const jsonAnswer = await answer.json();
     console.log(jsonAnswer);
     updateMap(jsonAnswer)
-
+    return jsonAnswer;
 }
 
-async function get_Name(playerName) {
-    const nameAnswer = await fetch(`http://127.0.0.1:3000/newplayer/${playerName}`)
-    const data = await nameAnswer.text()
-    console.log(data);
+
+async function addPlayer() {
+    const playerName = prompt("Enter your name :");
+    const startIsoCode = prompt('Enter the starting country code:');
+
+    const airportData = await airportByIsocode(startIsoCode);
+    await fetch("http://127.0.0.1:3000/newplayer", {
+        method: "POST",
+        body: JSON.stringify({airport: airportData, playerName: playerName})
+    });
+    const airportResponse = await fetch("http://127.0.0.1:3000/airport/6")
+    const airportResponseJson = await airportResponse.json()
+    console.log(airportResponseJson)
+    const buttons = document.querySelectorAll('.button')
+    airportResponseJson.forEach((value,index)=>{
+        console.log(value,index, buttons[index])
+        const button = buttons[index]
+        button.querySelector("p").textContent = value[1]
+    })
 }
- get_Name(playerName)
 
 
 function updateMap(jsonAnswer) {
     const longitude_deg = jsonAnswer.longitude_deg
     const latitude_deg = jsonAnswer.latitude_deg
     map.setView([latitude_deg, longitude_deg], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {attribution: ""}).addTo(map);
 
     L.marker([latitude_deg, longitude_deg]).addTo(map)
         .bindPopup(jsonAnswer.name)
         .openPopup();
 }
 
-
-
-
-
-kutsu(airportCountry);
-
-
-
-
-
+addPlayer()
--- main.py
+++ main.py
@@ -1,48 +1,79 @@
 import random
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, request
 import mysql.connector
-
-
-class Player:
-
-    def __init__(self, id, nimi, airport, co2, score):
-        self.id = id
-        self.nimi = nimi
-        self.airport = airport
-        self.co2 = co2
-        self.score = score
-        self.co2max = 100000
+import requests
 
 
 connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    database='flight_game',
+    database='flight_game1',
     user='root',
-    password='portland',
+    password='Rommikola77',
     autocommit=True
 )
+cursor = connection.cursor(buffered=True)
+
+class Player:
+
+    def __init__(self, nimi, airport):
+        cursor.execute("SELECT max(CAST(id AS INT)) FROM game")
+        id_result = cursor.fetchone()[0]
+        if id_result == None:
+            self.id = 1
+        else:
+            self.id = int(id_result) + 1
+        self.nimi = nimi
+        self.airport = airport
+        self.co2 = 0
+        self.score = 0
+        self.co2max = 100000
+
+
 
 app = Flask(__name__)
 CORS(app)
 app.config["cors_headers"] = "content-type"
 
 
-@app.route("/newplayer/<name>")
-def give_name(name):
-    newplayer = Player(1, name, "", 0, 0)
-    print(newplayer.nimi)
-    return "ok"
-
-
 @app.route("/code/<maakoodi>")
 def get_country(maakoodi):
     sql = f"SELECT name, iso_country, municipality, latitude_deg, longitude_deg FROM airport WHERE iso_country='{maakoodi}';"
-    cursor = connection.cursor()
     cursor.execute(sql)
     result = random.choice(cursor.fetchall())
     return {"icao": result[1], "name": result[0], "municipality": result[2], "latitude_deg": result[3], "longitude_deg": result[4]}
+
+
+@app.route("/newplayer", methods=["POST"])
+def new_player():
+    if request.method == "POST":
+        player_data = request.get_json(force=True)
+        print(player_data["playerName"])
+        print(player_data)
+        Player(player_data["playerName"], player_data["airport"])
+        return {"status": "ok"}
+
+cursor.execute("""
+select country.name, airport.name, ident, 
+  latitude_deg, longitude_deg, type from 
+    airport, country where 
+      airport.iso_country = country.iso_country""")
+all_airports = cursor.fetchall()
+
+@app.route("/airport/<numero>")
+def choose_airport(numero):
+    airport_buttons = []
+    while len(airport_buttons) < int(numero):
+        airport = random.choice(all_airports)
+        for s_airport in airport_buttons:
+            if s_airport[0] == airport[0]: break
+            if airport[5] == "closed" and s_airport[5] == "closed": break
+        else:
+            airport_buttons.append(airport)
+    return airport_buttons
+
+
 
 
 if __name__ == "__main__":
