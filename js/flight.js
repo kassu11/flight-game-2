643ef0dbdda625ca954dcd81d73db9d7654d5c59
Jambolul <jami.naatanen@gmail.com>
2022-12-05 11:50:36 +0200

    Käyttöliittymän parantamista [#9]
    - Lisätty kartan ja nappien päälle score,
      pelaaja, lentokenttä, co2 tiedot
    - Näitä myös tyylitelty jonkin verran
    - Lisätty custom fontti
    - Luotu numeroille tyylittelyfunktio

--- js/flight.js
+++ js/flight.js
@@ -1,8 +1,12 @@
-const map = L.map("map");
+const map = L.map('map');
 const allMarkers = [];
 let currentAirport = null;
-let currentPlayer = null
+let currentPlayer = null;
 
+Number.prototype.formatNumbers = function(e) {
+  return this.toLocaleString('ja-JP', {style: 'currency', currency: 'JPY'}).
+      slice(1);
+};
 
 async function airportByIsocode(airportCountry) {
   const answer = await fetch(`http://127.0.0.1:3000/code/${airportCountry}`);
@@ -15,16 +19,17 @@ async function airportByIsocode(airportCountry) {
   });
   return jsonAnswer;
 }
+
 async function enterStartingCountry() {
-  const startIsoCode = prompt("Enter the starting country code:");
-  if (startIsoCode.length == 0) return "random";
+  const startIsoCode = prompt('Enter the starting country code:');
+  if (startIsoCode.length == 0) return 'random';
   const isoResult = await fetch(
-    `http://127.0.0.1:3000/tarkista-maakoodi/${startIsoCode}`
+      `http://127.0.0.1:3000/tarkista-maakoodi/${startIsoCode}`,
   );
   const isoResultJson = await isoResult.json();
   if (isoResultJson.result == false) {
     const again = confirm(
-      "Isocode is not right, do you want to type a new one?"
+        'Isocode is not right, do you want to type a new one?',
     );
     if (again == true) {
       return enterStartingCountry();
@@ -34,54 +39,56 @@ async function enterStartingCountry() {
 }
 
 async function addPlayer() {
-  const playerName = prompt("Enter your name :");
+  const playerName = prompt('Enter your name :');
   const startIsoCode = await enterStartingCountry();
   const airportData = await airportByIsocode(startIsoCode);
 
-  const playerResponse = await fetch("http://127.0.0.1:3000/newplayer", {
-    method: "POST",
-    body: JSON.stringify({ airport: airportData, playerName: playerName }),
+  const playerResponse = await fetch('http://127.0.0.1:3000/newplayer', {
+    method: 'POST',
+    body: JSON.stringify({airport: airportData, playerName: playerName}),
   });
   const playerJson = await playerResponse.json();
   console.log(playerJson);
-  currentPlayer = playerJson 
+  currentPlayer = playerJson;
   getNewAirports(6);
 }
+
 async function getNewAirports(numb) {
   const airportResponse = await fetch(`http://127.0.0.1:3000/airport/${numb}`);
   const airportResponseJson = await airportResponse.json();
-  const buttons = document.querySelectorAll(".buttons button");
+  const buttons = document.querySelectorAll('.buttons button');
 
   airportResponseJson.forEach((airport, index) => {
     const button = buttons[index];
-    button.querySelector("span").textContent = airport[1];
+    button.querySelector('span').textContent = airport[1];
     button.onclick = async () => {
       const calculateResponse = await fetch(
-        `http://127.0.0.1:3000/laske-lennon-tiedot`,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            alkuLentokentta: currentAirport,
-            loppuLentokentta: airport,
-          }),
-        }
+          `http://127.0.0.1:3000/laske-lennon-tiedot`,
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              alkuLentokentta: currentAirport,
+              loppuLentokentta: airport,
+            }),
+          },
       );
       const calculateJson = await calculateResponse.json();
+      console.log(calculateJson.score);
       const playerResponse = await fetch(`http://127.0.0.1:3000/matkusta`, {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
           matka: calculateJson.matka,
           score: calculateJson.score,
           co2: calculateJson.co2,
           playerId: currentPlayer.id,
-          airport
-        })
+          airport,
+        }),
       });
 
-      const playerJson = await playerResponse.json()
-      currentPlayer = playerJson
+      const playerJson = await playerResponse.json();
+      currentPlayer = playerJson;
       console.log(currentPlayer);
-
+      updatePlayerInterface(currentPlayer);
       currentAirport = airport;
       removeAllMarkers();
       updateMap({
@@ -94,18 +101,29 @@ async function getNewAirports(numb) {
   });
 }
 
+function updatePlayerInterface(player) {
+  document.querySelector('#player .text').textContent = player.nimi;
+  document.querySelector('#score .text').textContent = player.score;
+  document.querySelector(
+      '#totalCo2 .text').textContent = `${player.co2.formatNumbers()} / 
+                    ${player.co2max.formatNumbers()}`;
+  document.querySelector(
+      '#airport .text').textContent = `[${player.airport[0]}] ${player.airport[1]}`;
+  console.log(player.airport);
+}
+
 function updateMap(jsonAnswer) {
   const longitude_deg = jsonAnswer.longitude_deg;
   const latitude_deg = jsonAnswer.latitude_deg;
   map.setView([latitude_deg, longitude_deg], 5);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "",
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '',
   }).addTo(map);
 
-  const marker = L.marker([latitude_deg, longitude_deg])
-    .addTo(map)
-    .bindPopup(jsonAnswer.name)
-    .openPopup();
+  const marker = L.marker([latitude_deg, longitude_deg]).
+      addTo(map).
+      bindPopup(jsonAnswer.name).
+      openPopup();
   allMarkers.push(marker);
 }
 
