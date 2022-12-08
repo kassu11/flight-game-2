2ccc7c98a7bd3bfbf21c339f2d4b230fc43f5824
adeahmed <98258192+adeahmed@users.noreply.github.com>
2022-12-08 14:53:05 +0200

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,10 +1,13 @@
 print("Testi")
 
 
-
-
-
-
-
-
-
+@app.route("/code/<maakoodi>")
+def get_country(maakoodi):
+    sql = f"select country.name, airport.name, ident, latitude_deg, longitude_deg, type from airport, country where airport.iso_country = country.iso_country and airport.iso_country='{maakoodi}';"
+    cursor.execute(sql)
+    sql_answer = cursor.fetchall()
+    if len(sql_answer) == 0:
+        result = random.choice(all_airports)
+        return list(result)
+    result = random.choice(sql_answer)
+    return list(result)
