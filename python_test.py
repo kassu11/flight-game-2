651630bc21cd2ac27895365a8c95c3d5c52019f3
Jambolul <jami.naatanen@gmail.com>
2022-12-08 14:58:41 +0200

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -44,3 +44,22 @@ def choose_airport(numero):
         else:
             airport_buttons.append(airport)
     return airport_buttons
+
+@app.route("/airport/<numero>")
+def choose_airport(numero):
+    airport_buttons = []
+    while len(airport_buttons) < int(numero):
+        airport = random.choice(all_airports)
+        for s_airport in airport_buttons:
+            if s_airport[0] == airport[0]:
+                break
+            if airport[5] == "closed" and s_airport[5] == "closed":
+                break
+            alkukentta = airport_buttons[-1][3:5]
+            nykynen_kentta = airport[3:5]
+            matka = geodesic(alkukentta, nykynen_kentta).km
+            if matka > 5000:
+                break
+        else:
+            airport_buttons.append(airport)
+    return airport_buttons
